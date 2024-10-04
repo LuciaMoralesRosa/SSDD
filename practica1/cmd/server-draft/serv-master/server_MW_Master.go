c148b53cb85bb8a7705e4a44245cfb7439f28dd6
Lucía <lucia.moralesrosa99@gmail.com>
2024-10-04 11:56:41 +0200

    Master worker, hay un error pero funciona ?

--- practica1/cmd/server-draft/serv-master/server_MW_Master.go
+++ practica1/cmd/server-draft/serv-master/server_MW_Master.go
@@ -18,6 +18,7 @@ import (
 	"os"
 	"os/exec"
 	"practica1/com"
+	"strconv"
 	"strings"
 )
 
@@ -54,18 +55,22 @@ func enviarTarea(ip string, interval com.TPInterval, id int) ([]int, error) {
 	return reply.Primes, nil
 }
 
-// go run server_MW_Master.go 192.168.3.2:8080 a816906 /practica1/cmd/server-draft/serv-worker/server_MW_Worker.go
+// go run server_MW_Master.go 192.168.3.2:8080 a816906 practica1/cmd/server-draft/serv-worker server_MW_Worker.go puerto
 func main() {
 	args := os.Args
-	if len(args) != 4 {
-		log.Println("Error: endpoint missing: go run serv_MW_Master.go ip:port usuario ruta       ")
+	if len(args) != 6 {
+		log.Println("Error: endpoint missing: go run serv_MW_Master.go ip:port usuario ruta fichero puerto")
 		os.Exit(1)
 	}
 	endpoint := args[1]
 
 	usuario := args[2] // a816906
-	ruta := args[3]    // /Practica1_G26/cmd/server-draft/serv-worker/server_MW_Worker.go
-	//ruta = ruta + "/server_MW_Worker.go"
+	ruta := args[3]    // cmd/server-draft/serv-worker
+	fichero := args[4] // server_MW_Worker.go
+	puerto, err := strconv.Atoi(args[5])
+	if err != nil {
+		log.Println("Error convirtuendo el puerto a entero:", err)
+	}
 
 	// Creacion del listener con la direccion proporcionada
 	listener, err := net.Listen("tcp", endpoint)
@@ -91,22 +96,28 @@ func main() {
 		com.CheckError(err)
 
 		//Definimos las maquinas trabajadoras
-		workers := []string{"192.168.3.3:8081",
-			"192.168.3.4:8082"}
+		workers := []string{fmt.Sprintf("192.168.3.3:%d", puerto),
+			fmt.Sprintf("192.168.3.3:%d", puerto+1)}
 
 		// Ruta del fichero a ejecutar
 
 		// Ejecución remota de los Workers
 		for i, worker := range workers {
-			rutaFichero := fmt.Sprintf("go run /misc/alumnos/sd/sd2425/%s%s 192.168.3.808%d", usuario, ruta, i)
-			// go run /misc/alumnos/sd/sd2425/a816906/Practica1_G26/cmd/server-draft/serv-worker/server_MW_Worker.go 192.168.3.8081
-			ipSinPuerto := strings.Split(worker, ":")[0]
-			cmd := exec.Command("ssh ", fmt.Sprintf("%s@%s", usuario, ipSinPuerto), rutaFichero)
+
+			// Creacion de comando con parametros
+			workerSinPuerto := strings.Split(worker, ":")[0]
+			comando := fmt.Sprintf("ssh %s@%s 'cd /misc/alumnos/sd/sd2425/%s/%s; go run %s %s:%d'", usuario, workerSinPuerto, usuario, ruta, fichero, workerSinPuerto, puerto+i)
+
+			// ssh a816906@192.168.3.3 'cd /misc/alumnos/sd/sd2425/a816906/practica1/cmd/server-draft/serv-worker; go run server_MW_Worker.go 192.168.3.3:8081'
+
+			//Creacion comando cutre
+			cmd := exec.Command(comando)
 			output, err := cmd.CombinedOutput() // Captura la salida y errores
 			if err != nil {
 				fmt.Printf("Error ejecutando en %s: %s\n", worker, err)
 			}
-			fmt.Printf("Salida de %s:\n%s\n", worker, output)
+			fmt.Printf("Salida de %s - (output) ->\n%s\n", worker, output)
+
 		}
 
 		var resultados []int
